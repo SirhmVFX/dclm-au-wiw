d0dc280134a74833d39dd3c103c2d277f0e6b440
Fullstack Mechanic <sirhmvfx@gmail.com>
2026-05-25 11:43:05 +0100

    made button

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,5 +1,25 @@
-function Button() {
-  return <div>Button</div>;
+import Link from "next/link";
+
+function Button({
+  href,
+  text,
+  type,
+}: {
+  href: string;
+  text: string;
+  type: string;
+}) {
+  return (
+    <>
+      <Link href={href}>
+        <button
+          className={`${type === "fill" ? "bg-orange-500 text-white" : "border-orange-500 border text-orange-500"}  px-6 font-bold py-3 rounded-full hover:bg-orange-600 transition-all duration-300 cursor-pointer`}
+        >
+          {text}
+        </button>
+      </Link>
+    </>
+  );
 }
 
 export default Button;
